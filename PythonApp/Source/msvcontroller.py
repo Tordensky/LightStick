1b841f5d3948e020e8e8786f39efb5efe1a75e06
Simon Nistad <simon.nistad@hotmail.com>
2013-09-11 18:58:33 +0200

    Added functionality for controlling MSV from beat controller. Now possible to trigger time from both BPM controller A and B

--- PythonApp/Source/msvcontroller.py
+++ PythonApp/Source/msvcontroller.py
@@ -1,4 +1,5 @@
 # Based on the code written by Ingar Arntzen, Norut & Motion Corporation
+from collections import defaultdict
 import os
 
 import threading
@@ -16,7 +17,10 @@ class SimpleMsvController(Widget):
     HOST = "mcorp.no:8091"
     MSVID = 18
 
+    msvVelocity = NumericProperty(0.0)
     msvPosition = NumericProperty(0.0)
+    msvAcceleration = NumericProperty(0.0)
+    currentBPM = NumericProperty(0.0)
 
     def __init__(self, **kwargs):
         super(SimpleMsvController, self).__init__(**kwargs)
@@ -29,13 +33,30 @@ class SimpleMsvController(Widget):
         #self.update(18, 0, 1, 0)
 
         self.lock = threading.RLock()
-
         self.msvThread = threading.Thread(target=self.run)
-
         self.msvThread.start()
 
+        self.source = None
+        self.bpms = defaultdict(float)
+
+    def setSourceForBpm(self, source):
+        self.source = source
+        self.setVelocityFromBPM(self.bpms[source])
+
+    def setBpm(self, bpm, source):
+        self.bpms[source] = bpm
+        print bpm, source
+
+    def setVelocityFromBPM(self, bpm):
+        print "Set new velocity"
+        velocity = bpm / 60.0
+        self.currentBPM = round(bpm, 1)
+        self.update(18, 0, velocity, 0)
+
+    def on_msvVelocity(self, *args):
+        print args[1]
+
     def update_handler(self, msv_data):
-        #print "Update", msv_data
         pass
 
     def update(self, msvid, p=None, v=None, a=None):
@@ -51,16 +72,17 @@ class SimpleMsvController(Widget):
     def join(self):
         self._client.join()
 
-    def updateScreen(self, value):
+    def updateScreen(self, pos, vel, acc):
         with self.lock:
-            self.msvPosition = round(value, 3)
+            self.msvPosition = round(pos, 3)
+            self.msvVelocity = round(vel, 3)
+            self.msvAcceleration = round(acc, 3)
 
     def run(self, *args):
         while not self._stop_event.is_set():
             vector = self._msv.query()
-            #print vector
 
-            self.updateScreen(vector[0])
+            self.updateScreen(vector[0], vector[1], vector[2])
 
             time.sleep(0.1)
 
