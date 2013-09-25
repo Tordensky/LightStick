ae2e93db359118d46cac83c63fb9eed41ba9f749
Simon Nistad <simon.nistad@hotmail.com>
2013-09-25 15:37:50 +0200

    Refactored color picker to use playback handler IN THIS COMMIT: - Removed allot of unnecessary code from the color mixer and inserted a playback handler instead - Some overall renaming of methods and variables
    KNOWN BUGS:
    - Gets a maximum recursion Error when activating sync fade and scene time. Probably a bug from the fader controller
    
    KNOWN TODO'S in controller code:
    - Strobe effects
    - To and from JSON for scenes
    - Refactor BPM controller
    - Finnish HTTP handler
    - Fix text sizes for buttons on scene controller

--- LightStickController/Source/ColorMixer/colorpicker.py
+++ LightStickController/Source/ColorMixer/colorpicker.py
@@ -1,8 +1,8 @@
 import os
-from kivy.clock import Clock
 from kivy.lang import Builder
-from kivy.properties import ListProperty, NumericProperty, BooleanProperty
+from kivy.properties import ListProperty, NumericProperty
 from kivy.uix.widget import Widget
+from SceneMixer import PlayBackHandler
 
 Builder.load_file(os.getenv("FILE_PATH") + "/colorpicker.kv")
 
@@ -16,110 +16,62 @@ class CustomWheel(Widget):
     screen_color = ListProperty((0.1, 0.5, 1, 1))
 
     bpm = NumericProperty(0.0)
-    trigger = BooleanProperty(False)
 
     fade_time_in_beats = NumericProperty(1.0)
-    steps_per_beat = NumericProperty(64)
 
     def __init__(self, **kwargs):
         super(CustomWheel, self).__init__(**kwargs)
-        self.new_color = (0, 0, 0, 0)
+        self.newColor = (0, 0, 0, 0)
 
-        self._fade_step_in_time = 0.0
-        self._remaining_fade_time = 0.0
+        self.__color_step_size = [0.0, 0.0, 0.0]
 
-        self.execute_change_color = False
-        self.color_step_size = [0.0, 0.0, 0.0]
+        self.__updatesPerBeat = 64
 
-    def on_color(self, instance, new_color):
-        self.new_color = new_color
-        #if self.trigger:
-        self.set_new_color()
-        #else:
-        #    self.execute_change_color = True
+        self.__playBackHandler = PlayBackHandler(bpm=60.0, updatesPerBeat=self.__updatesPerBeat)
+        self.__playBackHandler.addIntervalUpdateCallback(self.fadeStepCallback)
 
-    # TRIGGER SIGNAL FROM BPM COUNTER OR OTHER CONTROLLING UNIT
-    # def on_trigger(self, *args):
-    #     if args[1]:
-    #         if self.execute_change_color:
-    #             self.execute_change_color = False
-    #             self.set_new_color()
+    def on_bpm(self, obj, newBpm):
+        self.__playBackHandler.setBpm(newBpm)
 
-    def _trigger_update_color(self, idx, new_color_value):
+    def on_color(self, obj, newColor):
+        self.set_new_color(newColor)
+
+    def set_new_color(self, newColor):
+        self.newColor = newColor
+        self.__set_color_step_sizes()
+        self.__playBackHandler.start()
+
+    def fadeStepCallback(self, time):
+        if self.fade_time_in_beats <= time:
+            self.__playBackHandler.stopAndReset()
+        else:
+            self.__update_color_one_fade_step()
+
+    def setValueFromSlider(self, idx, new_color_value):
         color_value = new_color_value / 255.0
         if color_value > 1.0:
             color_value = 1.0
         self.color[idx] = color_value
 
-    def _calc_fade_time_in_time(self):
+    def __calc_number_of_steps(self):
+            return float(self.fade_time_in_beats) * self.__updatesPerBeat
+
+    def __get_color_step_size(self, idx):
         try:
-            return (float(self.fade_time_in_beats) / float(self.bpm)) * 60.0
-        except ZeroDivisionError:
-            print "ZERO DIV ERROR fade time"
-            return 0.0
-
-    # TODO does not return the correct result!
-    def _calc_fade_step_in_time(self):
-        try:
-            beats_per_sec = float(self.bpm) / 60.0
-            steps_size_in_time = float(beats_per_sec) / float(self.steps_per_beat)
-
-            fade_time = self._calc_fade_time_in_time()
-            if steps_size_in_time > fade_time:
-                return fade_time
-            return steps_size_in_time
-        except ZeroDivisionError:
-            print "ZERO DIV ERROR fade step"
-            return 0.0
-
-    def _calc_number_of_steps(self):
-        try:
-            return self.fade_time_in_time / self._fade_step_in_time
-        except ZeroDivisionError:
-            print "ZERO DIV ERROR num step"
-            return 0.0
-
-    def set_new_color(self):
-        self.fade_time_in_time = self._calc_fade_time_in_time()
-        self._fade_step_in_time = self._calc_fade_step_in_time()
-
-        self._remaining_fade_time = self.fade_time_in_time
-
-        self._set_color_step_sizes()
-
-        # TODO fix if fade step time is larger than fade time
-        Clock.unschedule(self._execute_fade_step)
-        Clock.schedule_once(self._execute_fade_step, self._fade_step_in_time)
-
-    def _execute_fade_step(self, *args):
-        if self._is_in_color_fade():
-            self._update_color_one_fade_step()
-            Clock.schedule_once(self._execute_fade_step, self._fade_step_in_time)
-        else:
-            self.screen_color = self.new_color
-
-    def _is_in_color_fade(self):
-        if self._remaining_fade_time <= 0.0:
-            return False
-        self._remaining_fade_time -= self._fade_step_in_time
-        return self._remaining_fade_time >= 0.0
-
-    def _get_color_step_size(self, idx):
-        try:
-            color_diff = (self.new_color[idx] - self.screen_color[idx])
-            return float(color_diff) / float(self._calc_number_of_steps())
+            color_diff = (self.newColor[idx] - self.screen_color[idx])
+            return float(color_diff) / float(self.__calc_number_of_steps())
         except ZeroDivisionError:
             return 0.0
 
-    def _set_color_step_sizes(self):
-        self.color_step_size[self.IDX_RED] = self._get_color_step_size(self.IDX_RED)
-        self.color_step_size[self.IDX_GREEN] = self._get_color_step_size(self.IDX_GREEN)
-        self.color_step_size[self.IDX_BLUE] = self._get_color_step_size(self.IDX_BLUE)
+    def __set_color_step_sizes(self):
+        self.__color_step_size[self.IDX_RED] = self.__get_color_step_size(self.IDX_RED)
+        self.__color_step_size[self.IDX_GREEN] = self.__get_color_step_size(self.IDX_GREEN)
+        self.__color_step_size[self.IDX_BLUE] = self.__get_color_step_size(self.IDX_BLUE)
 
-    def _update_color_one_fade_step(self):
-        self._update_color_step_helper(self.IDX_RED)
-        self._update_color_step_helper(self.IDX_GREEN)
-        self._update_color_step_helper(self.IDX_BLUE)
+    def __update_color_one_fade_step(self):
+        self.__update_color_step_helper(self.IDX_RED)
+        self.__update_color_step_helper(self.IDX_GREEN)
+        self.__update_color_step_helper(self.IDX_BLUE)
 
-    def _update_color_step_helper(self, idx):
-        self.screen_color[idx] += self.color_step_size[idx]
+    def __update_color_step_helper(self, idx):
+        self.screen_color[idx] += self.__color_step_size[idx]
